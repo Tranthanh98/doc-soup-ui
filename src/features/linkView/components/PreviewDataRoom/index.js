8616473433e6e8a888f8331cb15db15a4d4a2eec
Thanh tran <trantienthanh2412@gmail.com>
2023-03-26 22:38:44 +0700

    fix node release

--- src/features/linkView/components/PreviewDataRoom/index.js
+++ src/features/linkView/components/PreviewDataRoom/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable max-lines */
+/* eslint no-unsafe-optional-chaining: ["error", { "disallowArithmeticOperators": true }] */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import RestService from 'features/shared/services/restService';
